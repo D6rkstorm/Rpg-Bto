6c1d9cfe21dfd0dd4c934a29f66642ed0f5a3706
D6rkstorm <spqr.dominus@gmail.com>
2018-05-01 20:10:00 -0500

    Added File Handling and Searching methods

--- bot.js
+++ bot.js
@@ -22,10 +22,9 @@ function Character() {
 
 var character = new Character();
 
-////////////////////////////////////////////////////////////
-
 var jsonString = JSON.stringify(character);
 
+//If you cannot figure this one out, you must be daft. Find another project please. 
 function createNewFile(fileName, message, channel) {
 	fs.appendFileSync("characters/" + fileName, message, function(err) {
 	    if(err) {
@@ -35,6 +34,7 @@ function createNewFile(fileName, message, channel) {
 	});
 }
 
+//Creates a new file if the file name is not found, or adds a new line at the end of the text document. Use createNewFile when creating new files however
 function appendFile(fileName, message, channel) {
 	fs.appendFileSync("characters/" + fileName, message + "\n", function(err) {
 	    if(err) {
@@ -44,54 +44,81 @@ function appendFile(fileName, message, channel) {
 	});
 }
 
+//Checks to see if the file with the name specified does exist
 function doesExist(fileName) {
 	if (fs.existsSync("characters/" + fileName)) {return true;} 
 	else {return false;}
 }
 
-/*function fileList(msg) { //The directory still needs some figuring out
-	
-	});
-}
-
-/*
-function readFile(fileName, channel) {
-	fs.readFile(fileName, "utf8", function(err, data) {
-		if(err) {
-	    	channel.send("ERROR: " + err);
-	        return console.log("ERROR: " + err);
-	    }
-	    if(data != "") {
-		    var t = data.split('\n');
-		    for(var i = 0; i < t.length; i++) {
-		    	if(t[i] != "") {
-					channel.send(t[i]);
-		    	}
-		    }
-		} else {
-			channel.send("The file is empty!");
-			console.log("The file is empty!");
+//Function that creates a new file with the character name specified by the command in Discord, with the first line being the ID of the author that sent the message. 
+function createCharacter(msg,command) { 
+	var val = command.substring(3);
+		var isAlpha = function(ch){return ch.match(/[a-z]/i) === null};
+		if(isAlpha(val))
+		{
+			msg.channel.send("Invalid Character name, please make sure it is alpha characters")
 		}
-	});
+		else
+		{
+			var name = val + ".txt";
+			if(doesExist(name))
+				{msg.channel.send("That character already exists, please choose another name!");}
+			else {
+ 			createNewFile(name, "", msg.channel);
+ 			appendFile(name, "ID=" + msg.author.id, msg.channel);
+ 			msg.channel.send("Character created as " + name)
+ 			msg.channel.send("The file was saved!");
+    		console.log("The file was saved as " + name);
+			}
+		}
 }
 
-function resetFile(fileName, channel) {
-	fs.writeFile("save.txt", "", function(err) {
-	    if(err) {
-	    	channel.send("ERROR: " + err);
-	        return console.log("ERROR: " + err);
-	    }
-	    channel.send("The file was reset!");
-	    console.log("The file was reset!");
-	});
+//Method to check if the user that is requesting to modify the file is the same user that created the file
+function isUser(text, key) { //text is the file split by newline, and you take the first element of that string, the key is the ID of the user. 
+	var temp = text.split("\n");
+	var id = temp[0];
+	var tempID = "ID=" + key;
+	if(id === tempID)
+		return true;
+	else
+		return false;
 }
 
+//Method that goes through the files in the "characters" directory and finds the characters with the same ID as the author of the message"
+function fileSearchS(msg){
+	var filesFound = fs.readdirSync("characters/");
+	var specificFiles = [""];
+	for(var i = 0; i < filesFound.length; i++)
+	{
+		var temp = fs.readFileSync("characters/" + filesFound[i]);
+		temp = temp.toString("utf-8",0)
+		if(isUser(temp, msg.author.id))
+			specificFiles.push(filesFound[i])
+	}
+	if(specificFiles.length > 0)
+		specificFiles.splice(0,1);
+	var text = "";
+	for(var j = 0; j < specificFiles.length; j++)
+	{
+		text = specificFiles[j] + ', ' + text;
+	}
+	msg.reply("we found " + specificFiles.length + " file(s) that you have created! Use the \"modify\" command to edit them. The file names are ```" + text + "```");
+	text = "";
+}
 
-fs.open(fileName, 'w', function(err,file) {
-if(err) throw err;
-	console.log("Saved!");
+//Method that finds all the files in the "characters" directory, regardless of the user that created them
+function fileSearch(msg){
+	var filesFound = fs.readdirSync("characters/");
+	msg.channel.send("I found " + filesFound.length + " file(s)! The files names are:")
+	var names = ""
+	for(var i = 0; i < filesFound.length; i++)
+	{
+		names = filesFound[i] + ", " + names;
+	}
+	msg.channel.send("```" + names + "```")
+}
 
-		*/ 
+//method that checks to see if the author of the message is part of the trusted users group. 
 function trustCheck(message){
 	for(var i = 0; i < trustedUsers.length; i++)
 	{
@@ -151,33 +178,19 @@ client.on("message", (msg) => {
 			msg.channel.send("You rolled " + total + " in the end!")
 		}
  		
- 		if(command === "search"){
+ 		if(command === "searchMe"){
+ 			fileSearchS(msg);
+ 		}
+
+ 		if(command === "search") {
  			fileSearch(msg);
  		}
 
  		if(command.substring(0,2) === "cc"){
- 			var val = command.substring(3);
- 			var isAlpha = function(ch){return ch.match(/[a-z]/i) === null};
- 			if(isAlpha(val))
- 			{
- 				msg.channel.send("Invalid Character name, please make sure it is alpha characters")
- 			}
- 			else
- 			{
-	 			var name = val + ".txt";
-	 			if(doesExist(name))
-	 				{msg.channel.send("That character already exists, please choose another name!");}
-	 			else {
-		 			createNewFile(name, "", msg.channel);
-		 			appendFile(name, msg.author.id, msg.channel);
-		 			msg.channel.send("Character created as " + name)
-		 			msg.channel.send("The file was saved!");
-		    		console.log("The file was saved as " + name);
- 				}
- 			}	
+ 			createCharacter(msg,command);
  		}
 	}
 });
 
-client.login(kappa);
+client.login(a);
 client.on("ready", () => { console.log("bot is running!")});
